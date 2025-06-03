a83dc0d6b9e7259a1fc3a5f5de04ef9c3465047e
apiwit.p <apiwit2070@gmail.com>
2025-06-04 00:33:29 +0700

    Add GitHub link

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,14 @@
 export default function Header() {
   return (
-    <header className="w-full px-4 bg-white h-12 flex items-center shadow-sm font-bold">
-      Midi Visualizer
+    <header className="w-full px-4 bg-white h-12 flex justify-between items-center shadow-sm font-bold">
+      <p>Midi Visualizer</p>
+      <a
+        href="https://github.com/apiwitp2070/midi-visualizer"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src="github-mark.svg" width={24} height={24} />
+      </a>
     </header>
   );
 }
